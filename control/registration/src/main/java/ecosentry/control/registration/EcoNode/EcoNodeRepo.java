f8cae24d8f03da851c122d281ed3ace27040dc54
ByronBBanz <82751249+ByronBBanz@users.noreply.github.com>
2023-09-18 17:49:34 +1000

    Test

--- control/registration/src/main/java/ecosentry/control/registration/EcoNode/EcoNodeRepo.java
+++
@@ -1,7 +0,0 @@
-package ecosentry.control.registration.EcoNode;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EcoNodeRepo extends JpaRepository<EcoNode, Long> {
-    
-}
--- control/registration/src/main/java/ecosentry/control/registration/EcoNode/EcoNodeTokenRepo.java
+++
@@ -1,7 +0,0 @@
-package ecosentry.control.registration.EcoNode;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EcoNodeTokenRepo extends JpaRepository<NodeToken, Long> {
-    NodeToken findByToken(String token);
-}
