3628c2e804fae02b57c335551ab1e91950c24547
Eddie1308 <67546692+Eddie1308@users.noreply.github.com>
2023-10-03 15:54:38 +1100

    summary

---
+++ control/website/src/main/js/test.js
@@ -0,0 +1,20 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{
+render() {
+return (
+
+<div>Helloppppppppppppppp</div>
+
+);
+}
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('home')
+  );
