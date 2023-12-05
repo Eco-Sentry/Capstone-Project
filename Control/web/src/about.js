b9d9c4af16c96e47489b7e1a8c7290ac37bdf941
Savannah Fox <savfox11@hotmail.com>
2023-12-05 12:57:11 +1100

    Working on about page

--- Control/web/src/about.js
+++ Control/web/src/about.js
@@ -1,9 +1,28 @@
 function About() {
     return (
         <div>
-            <div>
-                <h2>ABOUT US</h2>
+            <div class="container-about">
+                <div class="h3-about">Welcome</div>
+                <div class="welcome-text-box">...</div>
+            
+                <div class="h3-about">About Us</div>
+                <div class="about-text-box">...</div>
             </div>
+
+            <div class="h3-about">Founders</div>
+            
+            <table class="center-table">
+                <tr>
+                    <td><div class="founder-text-box">Byron</div></td>
+                    <td><div class="founder-text-box">Esteban</div></td>
+                    <td><div class="founder-text-box">Eddie</div></td>
+                </tr>
+                <tr>
+                    <td><div class="founder-text-box">Savannah</div></td>
+                    <td><div class="founder-text-box">Nick</div></td>
+                    <td><div class="founder-text-box">Jena</div></td>
+                </tr>
+            </table> 
         </div>    
     );
 }
