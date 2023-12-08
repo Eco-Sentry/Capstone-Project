de0cc064db30bca0823190d11e881edf510c1bc5
Savannah Fox <savfox11@hotmail.com>
2023-12-08 14:40:16 +1100

    Change background and nav bar colours

--- Control/web/src/about.js
+++ Control/web/src/about.js
@@ -1,79 +1,82 @@
 function About() {
     return (
-        <div class="About-page">
-            <body>
-                <div class="container-about">
-                    <div class="h3-about">Welcome</div>
-                    <table class="about-welcome-table">
-                        <tr>
-                            <td class="welcome-table-c1">
-                                <div class="welcome-text-box">...</div>
-                            </td>
-                            <td class="welcome-table-c2">
-                                <iframe title="Welcome Video" height="400px" width="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
-                            </td>
-                        </tr>
-                    </table>
-                    <div class="h3-about">About Us</div>
-                    <div class="about-text-box">...</div>
-                    <div class="h3-about">Founders</div>
-                    <table class="center-table">
-                        <tr>
-                            <td class="center-tableRows">
-                                <div class="founder-text-box"> 
-                                    <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
-                                    <br></br> 
-                                    <h2>Byron</h2> 
-                                    <h3>Backend</h3>
-                                </div>
-                            </td>
-                            <td class="center-tableRows"> 
-                                <div class="founder-text-box"> 
-                                    <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
-                                    <br></br> 
-                                    <h2>Esteban</h2> 
-                                    <h3>Hardware</h3>
-                                </div>
-                            </td>
-                            <td class="center-tableRows">
-                                <div class="founder-text-box"> 
-                                    <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div> 
-                                    <br></br> 
-                                    <h2>Eddie</h2>
-                                    <h3>Backend</h3>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="center-tableRows">
-                                <div class="founder-text-box"> 
-                                    <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
-                                    <br></br> 
-                                    <h2>Savannah</h2>
-                                    <h3>Frontend</h3>
-                                </div>
-                            </td>
-                            <td class="center-tableRows">
-                                <div class="founder-text-box"> 
-                                    <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
-                                    <br></br> 
-                                    <h2>Nick</h2> 
-                                    <h3>Frontend</h3>
-                                </div>
-                            </td>
-                            <td class="center-tableRows">
-                                <div class="founder-text-box"> 
-                                    <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
-                                    <br></br> 
-                                    <h2>Jena</h2> 
-                                    <h3>Frontend</h3>
-                                </div>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-            </body> 
-        </div>    
+        <div className="body">
+            <div class="About-page">
+                <body>
+                    <div class="container-about">
+                        <div class="h3-about">Welcome</div>
+                        <table class="about-welcome-table">
+                            <tr>
+                                <td class="welcome-table-c1">
+                                    <div class="welcome-text-box">...</div>
+                                </td>
+                                <td class="welcome-table-c2">
+                                    <iframe title="Welcome Video" height="400px" width="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+                                </td>
+                            </tr>
+                        </table>
+                        <div class="h3-about">About Us</div>
+                        <div class="about-text-box">...</div>
+                        <div class="h3-about">Founders</div>
+                        <table class="center-table">
+                            <tr>
+                                <td class="center-tableRows">
+                                    <div class="founder-text-box"> 
+                                        <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
+                                        <br></br> 
+                                        <h2>Byron</h2> 
+                                        <h3>Backend</h3>
+                                    </div>
+                                </td>
+                                <td class="center-tableRows"> 
+                                    <div class="founder-text-box"> 
+                                        <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
+                                        <br></br> 
+                                        <h2>Esteban</h2> 
+                                        <h3>Hardware</h3>
+                                    </div>
+                                </td>
+                                <td class="center-tableRows">
+                                    <div class="founder-text-box"> 
+                                        <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div> 
+                                        <br></br> 
+                                        <h2>Eddie</h2>
+                                        <h3>Backend</h3>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="center-tableRows">
+                                    <div class="founder-text-box"> 
+                                        <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
+                                        <br></br> 
+                                        <h2>Savannah</h2>
+                                        <h3>Frontend</h3>
+                                    </div>
+                                </td>
+                                <td class="center-tableRows">
+                                    <div class="founder-text-box"> 
+                                        <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
+                                        <br></br> 
+                                        <h2>Nick</h2> 
+                                        <h3>Frontend</h3>
+                                    </div>
+                                </td>
+                                <td class="center-tableRows">
+                                    <div class="founder-text-box"> 
+                                        <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
+                                        <br></br> 
+                                        <h2>Jena</h2> 
+                                        <h3>Frontend</h3>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </body> 
+            </div>
+        </div>
+            
     );
 }
 
