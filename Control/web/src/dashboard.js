e9504a20d1cac44aa35f19b21e67b424413ed9e4
Savannah Fox <savfox11@hotmail.com>
2023-12-10 11:24:25 +1100

    starting dashboard

--- Control/web/src/dashboard.js
+++ Control/web/src/dashboard.js
@@ -2,7 +2,18 @@ function Dashboard() {
     return (
         <div>
             <div className='body'>
-            <h2>DASHBOARD</h2>
+                <div class="dashboard-sidebar">
+                    <div class="dashboard-logoCircle"> <img src="logo.jpg" alt="logo" /> </div>
+                    <a></a>
+                    <a >Sentries</a>
+                    <a >Create Sentries</a>
+                    <a >Groups</a>
+                </div>
+                <div class="dashboard-main">
+                    <div class="dashboard-box">
+                        <h3>Test - must update depending on side bar</h3>
+                    </div>
+                </div>
             </div>
         </div>    
     );
