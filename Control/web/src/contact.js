add76d3e849a451bcc05b61eef7095f8759d89a5
Savannah Fox <savfox11@hotmail.com>
2024-01-25 11:01:27 +1100

    Working on contact page

--- Control/web/src/contact.js
+++ Control/web/src/contact.js
@@ -1,33 +1,17 @@
 function Contact() {
     return (
         <div className='body'>
-            <div>
-            <h2>Contact Page</h2>
-            </div>
-
-            <div>
-                <h3> Phone: 0469 6969 6969</h3>
-                <h3> Address: 2 Northfeilds AveAddress: 2 Northfeilds AveAddress: 2 Northfeilds AveAddress: 2 Northfeilds AveAddress: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
-                <h3> Address: 2 Northfeilds Ave</h3>
+            <table class="center-table">
+                
+                <tr class="center-tableRows">
+                    <td> <img src="location.jpg" width="500" height="350"></img> </td>
+                    <td> <div class="details-text-box">  <div class="h3-contact">Details:</div> <br></br>  <div class="h3-contact">Name:</div> <div class="details-info-boxes">XXXXX</div>  <div class="h3-contact">Phone:</div> <div class="details-info-boxes">0469 6969 6969</div>  <div class="h3-contact">Email:</div> <div class="details-info-boxes">XXXXXX</div>  <div class="h3-contact">Address:</div><div class="details-info-boxes">XXXXXX</div> </div> </td>
+                </tr>
+            </table> 
+            <div class="about-text-box">
+                <div class="h3-contact">Contact Us:</div>
+                <div class="h3-contact">Title:</div> <div class="details-info-boxes">XXXXX</div>
+                <div class="h3-contact">Enquiry:</div> <div class="details-info-boxes">XXXXX</div>
             </div>
         </div>    
     );
