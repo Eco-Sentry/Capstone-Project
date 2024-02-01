3f19b5803e5575a4c54ff772f418742171309d4f
Savannah Fox <savfox11@hotmail.com>
2024-02-01 11:16:22 +1100

    contact page continued
    fixing div placements etc

--- Control/web/src/contact.js
+++ Control/web/src/contact.js
@@ -2,10 +2,63 @@ function Contact() {
     return (
         <div className='body'>
             <table class="center-table">
-                
                 <tr class="center-tableRows">
                     <td> <img src="location.jpg" width="500" height="350"></img> </td>
-                    <td> <div class="details-text-box">  <div class="h3-contact">Details:</div> <br></br>  <div class="h3-contact">Name:</div> <div class="details-info-boxes">XXXXX</div>  <div class="h3-contact">Phone:</div> <div class="details-info-boxes">0469 6969 6969</div>  <div class="h3-contact">Email:</div> <div class="details-info-boxes">XXXXXX</div>  <div class="h3-contact">Address:</div><div class="details-info-boxes">XXXXXX</div> </div> </td>
+                    <td> 
+                        <div class="details-text-box"> 
+                        <div class="h4-contact">Details:</div> 
+                            <table class="contact-details-table">
+                                <tr>
+                                    <td class="contact-details-table-col1">
+                                        <div class="h3-contact">
+                                            Name:
+                                        </div> 
+                                    </td>
+                                    <td class="contact-details-table-col1">
+                                        <div class="details-info-boxes">
+                                            XXXXX
+                                        </div> 
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="contact-details-table-col1">
+                                        <div class="h3-contact">
+                                            Phone:
+                                        </div> 
+                                    </td>
+                                    <td class="contact-details-table-col1">
+                                        <div class="details-info-boxes">
+                                            0469 6969 6969
+                                        </div>  
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="contact-details-table-col1">
+                                        <div class="h3-contact">
+                                            Email:
+                                        </div> 
+                                    </td>
+                                    <td class="contact-details-table-col1">
+                                        <div class="details-info-boxes">
+                                            XXXXXX
+                                        </div>  
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="contact-details-table-col1">
+                                        <div class="h3-contact">
+                                            Address:
+                                        </div>
+                                    </td>
+                                    <td class="contact-details-table-col1">
+                                        <div class="details-info-boxes">
+                                            XXXXXX
+                                        </div>           
+                                    </td>
+                                </tr>
+                            </table>  
+                        </div> 
+                    </td>
                 </tr>
             </table> 
             <div class="about-text-box">
