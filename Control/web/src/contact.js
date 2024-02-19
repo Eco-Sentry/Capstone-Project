cb41ba1c21ff4258fd06659ebf356da571871972
Savannah Fox <savfox11@hotmail.com>
2024-02-19 12:10:44 +1100

    contact page
    - added a map
    - trying to change the style

--- Control/web/src/contact.js
+++ Control/web/src/contact.js
@@ -3,10 +3,10 @@ function Contact() {
         <div className='Contact'>
             <table class="contact-center-table">
                 <tr class="center-tableRows">
-                    <td> <img src="location.jpg" width="500" height="350" alt="map"></img> </td>
+                    <td> <iframe  class="contact-map" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Northfields%20Ave,%20Wollongong%20NSW%202522+(UOW)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" title="map"><a href="https://www.gps.ie/">Main Location</a></iframe></td>
                     <td> 
                         <div class="details-text-box"> 
-                            <div class="h4-contact">Details:</div> 
+                            <div class="h2-contact">Details:</div> 
                             <table class="contact-details-table">
                                 <tr>
                                     <td class="contact-details-table-col1">
@@ -59,9 +59,9 @@ function Contact() {
                 </tr>
             </table> 
             <div class="contact-enquiry-text-box">
-                <div class="h3-contact">Contact Us:</div>
-                <div class="h3-contact">Title:</div> <div class="details-info-boxes">XXXXX</div>
-                <div class="h3-contact">Enquiry:</div> <div class="details-info-boxes-enquiry">XXXXX</div>
+                <div class="h2-contact">Contact Us:</div>
+                <div class="h3-contact">Title:</div> <input class="details-info-boxes" type="text"></input>
+                <div class="h3-contact">Enquiry:</div> <input class="details-info-boxes-enquiry" type="text"></input>
                 <button class="contact-details-submit"> <div class="h4-contact"> Submit </div> </button>
             </div>
         </div>    
