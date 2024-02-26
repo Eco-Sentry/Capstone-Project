46b3ac8c3b01d0570833858571e6b95e50756aa1
Savannah Fox <savfox11@hotmail.com>
2024-02-26 12:50:10 +1100

    about + contact cont

--- Control/web/src/about.js
+++ Control/web/src/about.js
@@ -1,16 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
+const getWindowDimensions = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height
+    };
+};
+const useWindowDimensions = () => {
+    const [windowDimensions, setWindowDimensions] = useState(
+        getWindowDimensions()
+    );  
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }  
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);  
+    return windowDimensions;
+};
+
 function About() {
+    const { height, width } = useWindowDimensions();
+
     const [isByronShown, setByronIsShown] = useState(false);
     const [isEstebanShown, setEstebanIsShown] = useState(false);
     const [isEddieShown, setEddieIsShown] = useState(false);
     const [isSavannahShown, setSavannahIsShown] = useState(false);
     const [isNickShown, setNickIsShown] = useState(false);
     const [isJenaShown, setJenaIsShown] = useState(false);
-
+    const [isGroupTableIsShown, setGroupTableIsShown] = useState(true);
+    
     return (
-        <div className='About' style={{ height: "100vh"}}>
+        <div className='About'>
             <div class="About-page">
                 <div class="container-about">
+                    <div>
+                        width: {width} x height: {height}
+                    </div>
                     <table class="about-welcome-table">
                         <tr>
                             <td class="welcome-table-c1">
@@ -26,28 +53,33 @@ function About() {
                         <div class="h3-about2">About Us</div>
                         <div class="about-text-box">From a group of friends to a project team, we created this solution to the growing problem of lack of consintrated and free data. Many weather stations and data collectors span a great range but are also congragated to specific locaiotns such that ........  </div>
                     </div>
+                    
                     <div class="h3-about">Founders</div>
                     <div id="aboutPersonElement">
                         <table class="center-table">
                             <tr>
                                 <td class="center-tableRows">
-                                    <div class="founder-text-box" onMouseEnter={() => setByronIsShown(true)} onMouseLeave={() => setByronIsShown(false)}>
-                                        <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
-                                        <br></br> 
-                                        <h2>Byron</h2> 
-                                        <h3>Backend</h3>
+                                    <div class="containerByron">
+                                        <div class="founder-text-box" onMouseEnter={() => setByronIsShown(true) + setGroupTableIsShown(false)} onMouseLeave={() => setByronIsShown(false) + setGroupTableIsShown(true)}>
+                                            <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
+                                            <br></br> 
+                                            <h2>Byron</h2> 
+                                            <h3>Backend</h3>
+                                        </div>
                                     </div>
                                 </td>
                                 <td class="center-tableRows"> 
-                                    <div class="founder-text-box" onMouseEnter={() => setEstebanIsShown(true)} onMouseLeave={() => setEstebanIsShown(false)}> 
-                                        <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
-                                        <br></br> 
-                                        <h2>Esteban</h2> 
-                                        <h3>Hardware</h3>
+                                    <div class="containerByron">
+                                        <div class="founder-text-box" onMouseEnter={() => setEstebanIsShown(true) + setGroupTableIsShown(false)} onMouseLeave={() => setEstebanIsShown(false) + setGroupTableIsShown(true)}> 
+                                            <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
+                                            <br></br> 
+                                            <h2>Esteban</h2> 
+                                            <h3>Hardware</h3>
+                                        </div>
                                     </div>
                                 </td>
                                 <td class="center-tableRows">
-                                    <div class="founder-text-box" onMouseEnter={() => setEddieIsShown(true)} onMouseLeave={() => setEddieIsShown(false)}> 
+                                    <div class="founder-text-box" onMouseEnter={() => setEddieIsShown(true) + setGroupTableIsShown(false)} onMouseLeave={() => setEddieIsShown(false) + setGroupTableIsShown(true)}> 
                                         <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div> 
                                         <br></br> 
                                         <h2>Eddie</h2>
@@ -57,7 +89,7 @@ function About() {
                             </tr>
                             <tr>
                                 <td class="center-tableRows">
-                                    <div class="founder-text-box" onMouseEnter={() => setSavannahIsShown(true)} onMouseLeave={() => setSavannahIsShown(false)}> 
+                                    <div class="founder-text-box" onMouseEnter={() => setSavannahIsShown(true) + setGroupTableIsShown(false)} onMouseLeave={() => setSavannahIsShown(false) + setGroupTableIsShown(true)}> 
                                         <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
                                         <br></br> 
                                         <h2>Savannah</h2>
@@ -65,7 +97,7 @@ function About() {
                                     </div>
                                 </td>
                                 <td class="center-tableRows">
-                                    <div class="founder-text-box" onMouseEnter={() => setNickIsShown(true)} onMouseLeave={() => setNickIsShown(false)}> 
+                                    <div class="founder-text-box" onMouseEnter={() => setNickIsShown(true) + setGroupTableIsShown(false)} onMouseLeave={() => setNickIsShown(false) + setGroupTableIsShown(true)}> 
                                         <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
                                         <br></br> 
                                         <h2>Nick</h2> 
@@ -73,7 +105,7 @@ function About() {
                                     </div>
                                 </td>
                                 <td class="center-tableRows">
-                                    <div class="founder-text-box" onMouseEnter={() => setJenaIsShown(true)} onMouseLeave={() => setJenaIsShown(false)}> 
+                                    <div class="founder-text-box" onMouseEnter={() => setJenaIsShown(true) + setGroupTableIsShown(false)} onMouseLeave={() => setJenaIsShown(false) + setGroupTableIsShown(true)}> 
                                         <div class="about-profileCircle"> <img src="placeholder.jpg" alt="placeholder" /> </div>
                                         <br></br> 
                                         <h2>Jena</h2> 
--- Control/web/src/contact.js
+++ Control/web/src/contact.js
@@ -1,4 +1,28 @@
+import React, { useEffect, useState } from "react";
+const getWindowDimensions = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height
+    };
+};
+const useWindowDimensions = () => {
+    const [windowDimensions, setWindowDimensions] = useState(
+        getWindowDimensions()
+    );  
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }  
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);  
+    return windowDimensions;
+};
+
 function Contact() {
+    const { height, width } = useWindowDimensions();
+
     return (
         <div className='Contact'>
             <table class="contact-center-table">
@@ -6,7 +30,7 @@ function Contact() {
                     <td> <iframe  class="contact-map" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Northfields%20Ave,%20Wollongong%20NSW%202522+(UOW)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" title="map"><a href="https://www.gps.ie/">Main Location</a></iframe></td>
                     <td> 
                         <div class="details-text-box"> 
-                            <div class="h2-contact">Details:</div> 
+                            <h5>Details:</h5> 
                             <table class="contact-details-table">
                                 <tr>
                                     <td class="contact-details-table-col1">
@@ -59,9 +83,9 @@ function Contact() {
                 </tr>
             </table> 
             <div class="contact-enquiry-text-box">
-                <div class="h2-contact">Contact Us:</div>
+                <h5>Contact Us:</h5>
                 <div class="h3-contact">Title:</div> <input class="details-info-boxes" type="text"></input>
-                <div class="h2-contact">Enquiry:</div> <input class="details-info-boxes-enquiry" type="text"></input>
+                <div class="h3-contact">Enquiry:</div> <input class="details-info-boxes-enquiry" type="text"></input>
                 <br></br> 
                 <button class="contact-details-submit"> Submit </button>
             </div>
