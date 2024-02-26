58ab4c8a17cfc0bb6aca36cbb0762a1e24ec2578
Savannah Fox <savfox11@hotmail.com>
2024-02-26 13:14:21 +1100

    about & contact cont

--- Control/web/src/contact.js
+++ Control/web/src/contact.js
@@ -27,10 +27,10 @@ function Contact() {
         <div className='Contact'>
             <table class="contact-center-table">
                 <tr class="center-tableRows">
-                    <td> <iframe  class="contact-map" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Northfields%20Ave,%20Wollongong%20NSW%202522+(UOW)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" title="map"><a href="https://www.gps.ie/">Main Location</a></iframe></td>
-                    <td> 
+                    <td class="contact-center-table-col1"> <iframe  class="contact-map" width="90%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Northfields%20Ave,%20Wollongong%20NSW%202522+(UOW)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" title="map"><a href="https://www.gps.ie/">Main Location</a></iframe></td>
+                    <td class="contact-center-table-col2"> 
                         <div class="details-text-box"> 
-                            <h5>Details:</h5> 
+                            <div class="contact-h1">Details:</div> 
                             <table class="contact-details-table">
                                 <tr>
                                     <td class="contact-details-table-col1">
@@ -77,15 +77,17 @@ function Contact() {
                                     </td>
                                 </tr>
                             </table>  
+                            <br></br>
                             <button class="contact-details-submit"> Submit </button>
+                            <br></br>
                         </div> 
                     </td>
                 </tr>
             </table> 
             <div class="contact-enquiry-text-box">
-                <h5>Contact Us:</h5>
-                <div class="h3-contact">Title:</div> <input class="details-info-boxes" type="text"></input>
-                <div class="h3-contact">Enquiry:</div> <input class="details-info-boxes-enquiry" type="text"></input>
+                <div class="contact-h1">Contact Us:</div>
+                <div class="contact-h1">Title:</div> <input class="details-info-boxes" type="text"></input>
+                <div class="contact-h1">Enquiry:</div> <input class="details-info-boxes-enquiry" type="text"></input>
                 <br></br> 
                 <button class="contact-details-submit"> Submit </button>
             </div>
