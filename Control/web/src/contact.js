4678f9eee06ddcbfa6f9bb43be457184d38c8589
Savannah Fox <savfox11@hotmail.com>
2024-03-12 16:02:06 +1100

    Contact
    changed it to fit the newer design, just gotta do some last minute touches like fix positioning etc

--- Control/web/src/contact.js
+++ Control/web/src/contact.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import logo from './EcoSentryLogo.png'; 
 const getWindowDimensions = () => {
     const { innerWidth: width, innerHeight: height } = window;
     return {
@@ -19,7 +20,6 @@ const useWindowDimensions = () => {
     }, []);  
     return windowDimensions;
 };
-
 function Contact() {
     const { height, width } = useWindowDimensions();
 
@@ -27,92 +27,50 @@ function Contact() {
         <div className='Contact'>
             <div class='container-contact'>
                 <table class="contact-center-table">
-                    <tr class="center-tableRows">
-                        <td class="contact-center-table-col1"> <iframe  class="contact-map" width="90%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Northfields%20Ave,%20Wollongong%20NSW%202522+(UOW)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" title="map"><a href="https://www.gps.ie/">Main Location</a></iframe></td>
-                        <td class="contact-center-table-col2"> 
-                            <div class="details-text-box"> 
-                                <div class="contact-h1">Details:</div> 
-                                <table class="contact-details-table">
-                                    <tr>
-                                        <td class="contact-details-table-col1">
-                                            <div class="h3-contact">
-                                                Name:
-                                            </div> 
-                                        </td>
-                                        <td class="contact-details-table-col2">
-                                            <input class="details-info-boxes-input" type="text">  
-                                            </input>  
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="contact-details-table-col1">
-                                            <div class="h3-contact">
-                                                Phone:
-                                            </div> 
-                                        </td>
-                                        <td class="contact-details-table-col2">
-                                            <input class="details-info-boxes-input" type="text" >  
-                                            </input>   
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="contact-details-table-col1">
-                                            <div class="h3-contact">
-                                                Email:
-                                            </div> 
-                                        </td>
-                                        <td class="contact-details-table-col2">
-                                            <input class="details-info-boxes-input" type="text" >  
-                                            </input>   
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="contact-details-table-col1">
-                                            <div class="h3-contact">
-                                                Address:
-                                            </div>
-                                        </td>
-                                        <td class="contact-details-table-col2">
-                                            <input class="details-info-boxes-input" type="text" >  
-                                            </input>          
-                                        </td>
-                                    </tr>
-                                </table>  
-                                <br></br>
-                                <button class="contact-details-submit"> Submit </button>
-                                <br></br>
-                            </div> 
+                    <tr>
+                        <td>
+                            <img src={logo} alt="logo" class="center" height={200} width={200} style={{ alignSelf: 'center'}}/>   
+                            <h1>EcoSentry</h1>
+                            <h2>Contact</h2>
+                            <p>enquire@ecosentry.com.au</p>
+                            <h2>Address</h2>
+                            <p>12 Northfields ave</p>
+                            <p>Wollongong, NSW 2500</p>
+                        </td>
+                        <td>
+                            <table class="contact-get-in-touch-table">
+                                <tr>
+                                    <h1>Get in touch</h1>
+                                </tr>
+                                <tr height="15px">
+                                    <input class="details-info-boxes" type="text" placeholder="Name"></input>
+                                </tr>
+                                <tr height="15px">
+                                    <input class="details-info-boxes" type="text" placeholder="Phone"></input> 
+                                </tr>
+                                <tr height="15px">
+                                    <input class="details-info-boxes" type="text" placeholder="Email"></input>  
+                                </tr>
+                                <tr height="15px">
+                                    <input class="details-info-boxes" type="text" placeholder="Address"></input>   
+                                </tr>
+                                <tr height="15px">
+                                    <input class="details-info-boxes" type="text" placeholder="Title"></input>  
+                                </tr>
+                                <tr height="150px">
+                                    <input class="details-info-boxes" type="text" placeholder="Enquiry"></input>   
+                                </tr>
+                                <tr>
+                                    <br></br>
+                                    <button class="contact-details-submit"> Submit </button>
+                                </tr>
+                            </table>
                         </td>
                     </tr>
-                </table> 
-                <div class="contact-enquiry-text-box">
-                <div class="contact-enquiry-title">Contact Us:</div>
-                    <table class="contact-enquiry-text-box-table">
-                        <tr>
-                            <td>
-                                <table class="contact-details-table">
-                                    <tr>
-                                        <td><div class="h3-contact">Title:</div></td>
-                                        <td><input class="details-info-boxes" type="text"></input></td>
-                                    </tr>
-                                    <tr>
-                                        <td><div class="h3-contact">Your Name:</div> </td>
-                                        <td><input class="details-info-boxes" type="text"></input></td>
-                                    </tr>
-                                    <tr>
-                                        <td><div class="h3-contact">Email:</div></td>
-                                        <td><input class="details-info-boxes" type="text"></input></td>
-                                    </tr>
-                                </table>
-                            </td>
-                            <td>
-                                <div class="contact-h1">Enquiry:</div> <input class="details-info-boxes-enquiry" type="text"></input>
-                            </td>
-                        </tr>
-                    </table>
-                    <br></br> 
-                    <button class="contact-details-submit"> Submit </button>
-                </div>
+                    <tr><h1>Find Us Here</h1></tr>
+                </table>
+                <iframe  class="contact-map" position="center" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Northfields%20Ave,%20Wollongong%20NSW%202522+(UOW)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" title="map"><a href="https://www.gps.ie/">Main Location</a></iframe>
+                <br></br>
             </div>
         </div>
             
