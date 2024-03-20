23103791cf6566bd660e6d03bce906aabbc7972f
Savannah Fox <savfox11@hotmail.com>
2024-03-20 11:37:21 +1100

    home page
    changed it to be more like new figma
    old code gas been commentated out

--- Control/web/src/home.js
+++ Control/web/src/home.js
@@ -16,6 +16,7 @@ import PieChart from './components/PieChart';
 import './style.css';
 //About page 
 import React, { useState, useEffect  } from 'react';
+import { Chart } from 'chart.js';
 const getWindowDimensions = () => {
     const { innerWidth: width, innerHeight: height } = window;
     return {
@@ -37,22 +38,24 @@ const useWindowDimensions = () => {
     return windowDimensions;
 };
 //About page - end
-
-
-function Home() {
-  //About page 
-  const { height, width } = useWindowDimensions();
-
+/*
   const [isByronShown, setByronIsShown] = useState(false);
   const [isEstebanShown, setEstebanIsShown] = useState(false);
   const [isEddieShown, setEddieIsShown] = useState(false);
   const [isSavannahShown, setSavannahIsShown] = useState(false);
   const [isNickShown, setNickIsShown] = useState(false);
   const [isJenaShown, setJenaIsShown] = useState(false);
+*/ 
+
+function Home() {
+  //About page 
+  const { height, width } = useWindowDimensions();
+
+
   //About page - end
 
   return (
-      <div className='body'>
+    <div className='body'>
           <div className='section'>
             <div className='image'>
               <div class='row'>
@@ -70,7 +73,67 @@ function Home() {
               </div>
             </div>
           </div>
-          <div className='section'>
+          <table class="homePage-ourMission-table">
+            <tr>
+                <td class="homePage-ourMission-table-c1">
+                    <h1>Our mission</h1>
+                </td>
+                <td class="homePage-ourMission-table-c2">
+                    <p>
+                        Our software will facilitate seamless integration into the mesh network. Users can conveniently add their station to the network and gain access to a wealth of aggregated data presented through maps, raw data, and other informative visualizations. This collaborative effort will foster the generation of detailed, localized, and frequent environmental data in various regions across the globe.
+
+                        To achieve the utmost utility, we intend to design modules to accommodate a diverse range of sensors. These sensors will encompass crucial environmental parameters such as wind speed, wind direction, rainfall, CO2, CO, PM2.5, PM10, temperature, humidity, UV, pesticide levels, methane, NO2, nitric oxide, nitrogen dioxide, carbon monoxide, sulfur dioxide, ozone, radon gas, ammonia, air radiation levels, visibility, ambient temperature, humidity, and more. By collecting data from this wide array of sensors, we can gain comprehensive insights into the state of the environment and identify potential environmental issues early on.
+
+                        Ultimately, the EcoSentry project aims to empower individuals and communities with a powerful tool to monitor and protect their environment actively. By sharing and analyzing data on a global scale, we can contribute to a more sustainable and environmentally aware future for our planet. Together, we can create a world where localized and precise environmental information is readily available, driving informed decisions and positive actions for a greener and healthier Earth.
+                    </p>
+                </td>
+            </tr>
+          </table>
+          <h1>Charts and Graphs</h1>
+          <table class="charts-graphs-table">
+            <tr>
+                <td class="charts-graphs-table-c1">
+                    <BarChart />
+                </td>
+                <td class="charts-graphs-table-c2">
+                    <DoughnutChart />
+                </td>
+            </tr>
+            <tr>
+                <td class="charts-graphs-table-c1">
+                    <PieChart />
+                </td>
+                <td class="charts-graphs-table-c2">
+                    <BarChart />
+                </td>
+            </tr>
+          </table>
+          <table class="homePage-aboutUs-table">
+            <tr>
+                <td class="homePage-aboutUs-table-c1">
+                    <p>From a group of friends to a project team, 
+                        we have created this solution to the growing problem 
+                        of a lack of consintrated and free data. 
+                        Many weather station and data collectors span a 
+                        great range but are also congragated to specific Locations 
+                        such that......
+                    </p>
+                </td>
+                <td class="homePage-aboutUs-table-c2">
+                    <h1>About Us</h1>
+                </td>
+            </tr>
+          </table>  
+    </div>  
+    );
+}
+
+
+
+/*
+
+
+<div className='section'>
             <div class='row'>
               <div class='column'>
                 <div className='boxOne'>
@@ -109,8 +172,11 @@ function Home() {
             </div>
           </div>
 
-        
-        <div class="About-page">
+
+
+
+
+<div class="About-page">
             <div class="container-about">
                 <h2>Founders</h2>
                 <div id="aboutPersonElement">
@@ -176,13 +242,8 @@ function Home() {
                 
             </div>
         </div>
-    </div>  
-    );
-}
 
 
-
-/*
 {isByronShown && (
                     <div class="aboutUs-text-box">
                         <table class="aboutUs-table">
